7f248e60efdacf7ee4cc9bfb58fbc73d7b33f045
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-08 22:34:24 +0000

    fixing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
-sudo apt update && sudo apt install nodejs npm
+set -e
+echo "Start deployment"
+
+echo "Update the system and install Node.js"
+sudo apt update && sudo apt install nodejs 
+
+echo "Installing PM2"
 sudo npm install -g pm2
 
+echo "Stopping existing application"
 pm2 stop simple_app || true
+echo "Navigating to application directory"
 cd SimpleApplication_DevOps_CA/
 
+echo "Installing dependencies"
 npm install
 
+echo "Starting application"
 pm2 start app.js --name simple_app
+
+echo "Deployment successful"
