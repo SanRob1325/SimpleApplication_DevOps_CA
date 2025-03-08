7c034825c9a5f2be9aded028fe4d5059996d17bd
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-08 22:19:41 +0000

    Adding initial deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 
-pm2 stop simple_app
+pm2 stop simple_app || true
 cd SimpleApplication_DevOps_CA/
 
 npm install
 
+pm2 start app.js --name simple_app
