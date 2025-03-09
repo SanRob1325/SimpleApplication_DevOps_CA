f332a4109156a75da76f6a56fff74ba87c85493a
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-09 18:15:46 +0000

    removed docker for the time being

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,18 @@ echo "Start deployment"
 
 echo "Update the system and install Node.js"
 sudo dnf update -y
-sudo dnf install -y nodejs npm docker
+sudo dnf install -y nodejs npm
 
-echo "Starting Docker service"
-sudo systemctl start docker
-sudo systemctl enable docker
-
-echo "login Docker"
-echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "Installing PM2"
+sudo npm install -g pm2
 
 echo "Stopping existing application"
-sudo docker stop simple_app_container || true
-sudo docker rm simple_app_container || true
+pm2 stop simple_app || true
 
-echo "Pulling latest Docker container"
-sudo docker pull $DOCKER_USERNAME/$IMAGE_NAME:latest
+echo "Installing dependencies"
+npm install
 
-sudo docker run -d -p 8080:8080 -p 8443:8443 --name simple_app_container \
-    -v $(pwd)/privatekey.pem:/app/privatekey.pem \
-    -v $(pwd)/server.crt:/app/server.crt \
-    $DOCKER_USERNAME/$IMAGE_NAME:latest
-    
+echo "Starting application"
+pm2 start ./bin/www --name simple_app
+
 echo "Deployment successful"
